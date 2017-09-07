18c920082026b8732d5a7fa18ee5de9677b57669
Cheston <Cheston@DESKTOP-0BP8DKA>
2017-09-07 22:57:04 +1000

    main servlet

--- src/servlets/MainAPI.java
+++ src/servlets/MainAPI.java
@@ -1,5 +1,38 @@
 package servlets;
 
-public class MainAPI {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Main Servlet which controls what the system does
+ */
+@WebServlet("/API")
+public class MainAPI extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public MainAPI() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getParameter("action");
+		if (action.equals("signup")){
+			
+		}else if (action.equals("activateAccount")){
+			
+		}
+
+	}
+	
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 }
+
